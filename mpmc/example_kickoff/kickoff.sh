c1a7ed6e7e032f215f514be44111c1d0ea35b5f4
khavernathy <dfranz@mail.usf.edu>
2017-03-03 17:28:37 -0500

    changed mpmc kickoff with good comments

--- mpmc/example_kickoff/kickoff.sh
+++ mpmc/example_kickoff/kickoff.sh
@@ -1,46 +1,49 @@
 #!/bin/bash
 
-current=$(pwd);
+# THIS IS AN EXAMPLE SCRIPT WHERE WE SIMULATE 
+# MULTIPLE SORBATES ON CIRCE IN ONE BIG COMMAND:
+# bash kickoff.sh
 
-for s in h2; do
-#for s in co2 ch4; do
-#for s in c2h2; do
-#for s in c2h2 c2h4 c2h6; do
+current=$(pwd); # saving the current directory so we can remember it later
+# this directory should contain the 3 essential files for 1 MPMC job:
+    # s_m.pqr files, where s=sorbate and m=model, e.g. c2h2_polar.pqr
+    # submit.sh, a template for CIRCE job submission
+    # mpmc.inp, a template for MPMC run
+
+for s in c2h2 c2h4 c2h6; do # sorbate models
+mkdir $s
 cd $s
-for t in 77 87;do
-#for t in 298; do 
-#mkdir $t"K";
+for t in 298 273; do  # temps in K 
+mkdir $t"K";
 cd $t"K";
-for m in BSSP BSS BUCH DL; do 
-#for m in polar nonpolar trappe; do
-#for m in polar nonpolar; do
- #   mkdir $m;
+for m in polar nonpolar; do # type of model
+mkdir $m;
 	cd $m 
 
-    onoroff="off";
-    if [[ "$m" == "polar" ]] || [[ "$m" == "BSSP"  ]]; then
+    onoroff="off"; # a variable to automatically set polarization on/off
+    if [[ "$m" == "polar" ]]; then
         onoroff="on";
     fi
 
-for pres in 0.05 0.1 0.2 0.4 0.6 0.8 1.0; do # 5.0 10.0 20.0 30.0 40.0 50.0 60.0; do
+for pres in 0.05 0.1 0.2 0.4 0.6 0.8 1.0; do # pres in atm.
 
-#	mkdir $pres
+	mkdir $pres
 	cd ./$pres; pwd
 	
 	# make the submission script
     awk -v pres="$pres" -v model="$m" -v temp="$t" -v polar="$onoroff" '{
-        sub(/YPRESY/, pres);
-        sub(/ZTEMPZ/, temp);
-        sub(/XMODELX/, model);
-        sub(/CPOLARC/, polar);
-       
+        gsub(/YPRESY/, pres);
+        gsub(/ZTEMPZ/, temp);
+        gsub(/XMODELX/, model);
+        gsub(/CPOLARC/, polar);
         print;
         }' $current/submit.sh > submit.sh
+    
     # make the input
 	awk -v pres="$pres" -v polar="$onoroff" -v sor="$s" '{
-		sub(/XPRESX/, pres);
-        sub(/CPOLARC/, polar);
-		sub(/XSORX/, sor);
+		gsub(/XPRESX/, pres);
+        gsub(/CPOLARC/, polar);
+		gsub(/XSORX/, sor);
         print;
 	}' $current/mpmc.inp > mpmc.inp	
 
@@ -48,12 +51,16 @@ for pres in 0.05 0.1 0.2 0.4 0.6 0.8 1.0; do # 5.0 10.0 20.0 30.0 40.0 50.0 60.0
 	cp $current/$s"_"$m".pqr" input.pqr
 
 	# SAVE AND RESTART
+    # uncomment the stuff to save some important data and then
+    # re-run from last restart.pqr checkpoint.
 	mkdir saved_data;
+    # =============================================
 	#cat *restar*.pqr >> saved_data/restart.pqr
 	#rm input.pqr
 	#cat *.restart-00000.pqr >>  input.pqr
 	#cat *traj*.pqr >> saved_data/traj.pqr
 	#cat runlog* >> saved_data/runlog.log
+    # =============================================
 
 	sbatch submit.sh
 	cd ../
