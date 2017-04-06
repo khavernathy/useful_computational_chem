20d0c07a614419181832b483dee557a797c664fd
khavernathy <dfranz@mail.usf.edu>
2017-04-06 14:32:00 -0400

    dipoles

--- mpmc/dipoles/2correlate_posits_dipoles.sh
+++ mpmc/dipoles/2correlate_posits_dipoles.sh
@@ -6,6 +6,8 @@
 lower_limit=0.175
 upper_limit=0.5
 
+
+
 for pres in 0.05; do # 0.2 0.4 0.6 0.8 1.0 5.0 10.0 20.0 30.0 40.0 50.0 60.0; do
 
 	cat $pres/*.dipole-00000.dat | awk {'print sqrt($1*$1 + $2*$2 + $3*$3)'} > $pres/dipole_mags.dat
@@ -18,11 +20,11 @@ for pres in 0.05; do # 0.2 0.4 0.6 0.8 1.0 5.0 10.0 20.0 30.0 40.0 50.0 60.0; do
 	cat $pres/tmp_dipole_posits_and_mags.dat | awk {'if ($5 > '$lower_limit' && $5 < '$upper_limit') {print ;}'} > dipole_xyz.xyz
 
     lines=$(cat dipole_xyz.xyz | grep -c "")
-    echo $lines >> dipole_points.xyz
+    echo $lines > dipole_points.xyz
     echo "" >> dipole_points.xyz
     cat dipole_xyz.xyz >> dipole_points.xyz
     rm dipole_xyz.xyz
-	
+	mv dipole_points.xyz dipole_points$lower_limit"_to_"$upper_limit".xyz"
 								
 	#rm $pres/tmp_di.dat	
 done
@@ -32,4 +34,4 @@ done
 #xmgrace 0.1/tmp_cutoff.dat
 #echo "xmgrace 0.1/dipole_hist.dat"; xmgrace 0.1/dipole_hist.dat
 echo "dipole magnitudes from "$lower_limit" to "$upper_limit" considered."
-echo "output to ./dipole_points.xyz"
+echo "output to ./dipole_points"$lower_limit"_to_"$upper_limit".xyz"
