5ac3703b6dccc0116e33dd219012636e9c5b7f79
Douglas M Franz <dfranz@mail.usf.edu>
2017-10-03 10:18:33 -0400

    add vmd draw arrow tcl function

---
+++ vmd/draw_arrow.tcl
@@ -0,0 +1,6 @@
+proc vmd_draw_arrow {mol start end} {
+    # an arrow is made of a cylinder and a cone
+     set middle [vecadd $start [vecscale 0.9 [vecsub $end $start]]]
+     graphics $mol cylinder $start $middle radius 0.15
+     graphics $mol cone $middle $end radius 0.25
+}
