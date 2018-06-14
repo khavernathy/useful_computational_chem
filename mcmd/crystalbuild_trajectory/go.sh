8d51a0e21c0160027b893c7c7a564d684f21e3c9
khavernathy <khaverim7@gmail.com>
2018-06-14 13:32:05 -0400

    update crystal traj maker

--- mcmd/crystalbuild_trajectory/go.sh
+++ mcmd/crystalbuild_trajectory/go.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-num_atoms=140
-num_frames=157
-traj_file="SIFSIX18Nibeta-pos-1.xyz"
+num_atoms=152
+num_frames=60
+traj_file="SIFSIX18Ni-pos-1.xyz"
 
 rm out*xyz
 rm crystaltraj.xyz
@@ -11,14 +11,14 @@ awk '/ '$num_atoms'/{n++}{print >"out" n ".xyz" }' $traj_file
 
 
 for x in `seq 1 $num_frames`; do
-echo $x
 awk -v num="$x" '{
 
     gsub(/XXX/,num);
     print;
 }' mcmd.inp > a.inp;
 
-~/mcmd/mcmd a.inp;
+echo $x " / " $num_frames
+~/mcmd/mcmd a.inp > /dev/null;
 cat crystalbuild.xyz >> crystaltraj.xyz
 
 
